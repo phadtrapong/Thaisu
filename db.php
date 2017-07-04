c79da387cab82bc605d02ed3b3d89cc97d07a019
phadtrapong <phadtrapong.s@gmail.com>
2017-07-04 22:01:44 +0700

    remove db config

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 	$host = "localhost";
-	$username = "u883493915_yanik";
-	$password = "fpAxDlNtswCe6xL";
-	$db_name = "u883493915_yanik";
+	$username = "";
+	$password = "";
+	$db_name = "";
 
-	$db = new PDO("mysql:host=localhost;dbname=u883493915_yanik", $username, $password);
+	$db = new PDO("mysql:host=localhost;dbname=", $username, $password);
         $db->exec("set names utf8");
 ?>
